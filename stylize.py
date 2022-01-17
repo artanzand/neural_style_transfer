5a5f00a664835c8f1aff9fd3138d183f2a1ec5dd
Artan Zandian <74034506+artanzand@users.noreply.github.com>
2022-01-16 19:51:34 -0800

    Add get_style_layers

--- stylize.py
+++ stylize.py
@@ -5,12 +5,13 @@
 and applies Neural Style Transfer on the content image to create a stylized rendering of the content
 image based on the texture and style of the style image.
 
-Usage: stylize.py --content=<image_path> --style=<csv_path> --save=<save_path>
+Usage: stylize.py --content=<image_path> --style=<csv_path> --save=<save_path> --similarity=<direction>
 
 Options:
 --content=<image_path>               file path of the content image - initial  
 --style=<csv_path>                   file path of the style image - target
 --save=<save_path>                   file path to save the stylized image 
+--similarity=<direction>             Whether the generated image is similar to "content", "style", "balanced"
 """
 
 import os
@@ -82,8 +83,50 @@ def get_layer_outputs(vgg, layer_names):
     return model
 
 
-def get_layer_weights():
+def get_style_layers(similarity="balanced"):
+    """
+    Assigns weights to style layer outputs to define whether the generated image 
+    is similar to "content", "style", or "balanced"
+
+    Parameters
+    ----------
+    similarity: str, optional
+        a string identifying the similarity to either content, style or both
+    
+    Returns
+    -------
+    STYLE_LAYERS
+        a list of tuples identifying the name of style layer along with their weights
+    """
+    if similarity == "balanced":
+        STYLE_LAYERS = [
+        ('block1_conv1', 0.2),
+        ('block2_conv1', 0.2),
+        ('block3_conv1', 0.2),
+        ('block4_conv1', 0.2),
+        ('block5_conv1', 0.2)]
+    elif similarity == "content":
+        STYLE_LAYERS = [
+        ('block1_conv1', 0.02),
+        ('block2_conv1', 0.08),
+        ('block3_conv1', 0.2),
+        ('block4_conv1', 0.3),
+        ('block5_conv1', 0.4)]
+    elif similarity == "style":
+        ('block1_conv1', 0.4),
+        ('block2_conv1', 0.3),
+        ('block3_conv1', 0.2),
+        ('block4_conv1', 0.08),
+        ('block5_conv1', 0.02)]
+    else:
+        raise Exception("Please provide either of 'content', 'style' or 'balanced' for --similarity")
+
+    return STYLE_LAYERS
+
+
+
+
 
 
 if __name__ == "__main__":
-    main(opt["--content"], opt["--style"], opt["--save"])
+    main(opt["--content"], opt["--style"], opt["--save"], opt["--similarity"])
